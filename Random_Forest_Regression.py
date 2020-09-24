2d792a817e8915fff87cba65a36244fe1a69b17b
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-24 16:20:10 -0600

    Import libraries & dataset

--- Random_Forest_Regression.py
+++ Random_Forest_Regression.py
@@ -2,11 +2,18 @@
 # Input by Ryan L Buchanan 24SEP20
 
 # Import libraries
-
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # Import dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
 
+print(X)
 
+print(y)
 # Train the Random Forest Regression model on the whole dataset
 
 
