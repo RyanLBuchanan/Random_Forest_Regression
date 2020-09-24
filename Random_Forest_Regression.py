5bbe9e02c07d56cfe1729619ce387e99dbce4d6f
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-24 16:12:16 -0600

    Set up otline

--- Random_Forest_Regression.py
+++ Random_Forest_Regression.py
@@ -1,2 +1,16 @@
 # Random Forest Regression tutorial from Machine Learning A-Z - SuperDataScience
 # Input by Ryan L Buchanan 24SEP20
+
+# Import libraries
+
+
+# Import dataset
+
+
+# Train the Random Forest Regression model on the whole dataset
+
+
+# Predict a new result using specific instance of Years of Experience
+
+
+# Visualize the Random Forest Regression results (higher resolution)
